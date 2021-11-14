5eacb49ee0fd137f7cd712c75b0609893564ad22
Scarlet Vitoria <91432319+scar-vitoria@users.noreply.github.com>
2021-11-14 20:09:07 -0300

    Add files via upload

---
+++ mavenproject4/src/main/java/Passagem.java
@@ -0,0 +1,37 @@
+
+import javax.swing.JOptionPane;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vitor
+ */
+public class Passagem {
+    public static void main(String[] args) {
+        String destino;
+        int distancia, pedagios;
+        float var1, valorPassagem, valorKm, valorPedagio;
+        
+        destino = JOptionPane.showInputDialog("Informe o destino: ");
+        distancia = Integer.parseInt(JOptionPane.showInputDialog("Informe a distância em quilometros: "));
+        pedagios = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de pedágios: "));
+        valorKm = distancia * (45/100);
+        valorPedagio = pedagios * 9;
+        valor1 = ;
+        
+        System.out.println("Passageiro a passagem custa: " + );
+        
+        
+        
+                
+        
+        JOptionPane
+    }
+    
+}
